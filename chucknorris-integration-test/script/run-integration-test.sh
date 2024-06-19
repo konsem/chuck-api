4477c2a0f7b52556c30ff1a469e924db3652a438
Matías J. Schilling <m@matchilling.com>
2024-06-19 12:16:59 +0200

    Fix CORS issue

---
+++ chucknorris-integration-test/script/run-integration-test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+readonly SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+readonly MODULE_DIR=$(cd "${SCRIPT_DIR}/.." && pwd)
+readonly PROJECT_DIR=$(cd "${SCRIPT_DIR}/../.." && pwd)
+
+./gradlew --build-file "$PROJECT_DIR/build.gradle" clean dockerTagCurrent
+
+# docker-compose --file "$MODULE_DIR/docker-compose.yml" up \
+#   --exit-code-from "integration-test" \
+#   --quiet-pull \
+#   --remove-orphans
+
+docker-compose --file "$MODULE_DIR/docker-compose.yml" up \
+  --quiet-pull \
+  --remove-orphans
